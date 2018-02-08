c6d7a35b626dfc7ebd8b1a9d1c51662805d92dc5
totsaporn123 <totsaporn.pr.57@ubu.ac.th>
2018-02-08 21:12:37 +0700

    import data

---
+++ readxl.R
@@ -0,0 +1,7 @@
+#import data
+library(readxl)
+Nangrong <- read_excel("C:/Users/patsakon/Downloads/Nangrong.xlsx",  range = "A6:M36")
+View(Nangrong)
+#plot preciptation(y) with time(x)
+
+#save plot
